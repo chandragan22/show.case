36d13d9416363f75a399514bd046187869a93bde
Chandra <cgangavarapu22@gmail.com>
2017-07-23 05:20:43 -0500

    Log In

--- forms.py
+++ forms.py
@@ -9,3 +9,9 @@ class SignupForm(FlaskForm):
 	username = StringField('Username' , validators=[DataRequired("Please enter a username.")])
 	password = PasswordField('Password', validators=[DataRequired("Please enter a password."), length(min=6, message="Passwords must be at least 6 characters long.")])
 	submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired("Please enter your email address"), Email("Please enter a valid email address.")])
+	username = StringField('Username', validators=[DataRequired("Please enter your username.")])
+	password = PasswordField('Password', validators=[DataRequired("Please enter your password"), length(min=6, message="Passwords must be at least 6 characters long.")])
+	submit = SubmitField('Sign in')
--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
+#clapandtap
+#clapntap
+#tapandclap
 from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import SignupForm
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
@@ -18,6 +21,8 @@ def index():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+	if 'email' in session:
+		return redirect(url_for('home'))
 	form = SignupForm()
 
 	if request.method == 'POST':
@@ -29,14 +34,46 @@ def signup():
 			db.session.commit()
 	
 
-			session['username'] = newuser.username
+			session['email'] = newuser.email
 			return redirect(url_for('home'))
 
 	elif request.method == 'GET':
 		return render_template("signup.html", form=form)
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if 'email' in session:
+		return redirect(url_for('home'))
+	form = LoginForm()
+
+	if request.method == "POST":
+		if form.validate() == False:
+			return render_template("login.html", form=form)
+		else:
+			email = form.email.data
+			username = form.username.data
+			password = form.password.data
+
+			user = User.query.filter_by(email=email).first()
+			if user is not None and user.check_password(password):
+				session['email'] = form.email.data
+				return redirect(url_for('home'))
+			else:
+				return redirect(url_for('login'))
+
+	elif request.method == 'GET':
+		return render_template("login.html", form=form)
+
+@app.route("/logout")
+def logout():
+	session.pop('email', None)
+	return redirect(url_for('index'))
+
 @app.route("/home")
 def home():
+	if 'email' not in session:
+		return redirect(url_for('login'))
+
 	return render_template("home.html")
 
 @app.route("/music")
